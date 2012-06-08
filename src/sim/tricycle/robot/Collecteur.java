025de171f8a51c0ac9694fa91a1dd3c267eba1d2
Adrien Ortola <ortola66@gmail.com>
2012-06-08 15:00:33 +0200

    Structure Robot

---
+++ src/sim/tricycle/robot/Collecteur.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sim.tricycle.robot;
+
+/**
+ *
+ * @author Adri
+ */
+public class Collecteur extends $Robot{
+    
+    
+}
