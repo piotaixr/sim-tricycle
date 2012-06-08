b18ab045a43d9d0423515e6bc31bc9384bab152f
Augustin Husson <husson.augustin@gmail.com>
2012-06-08 11:02:17 +0200

    maj caml/automate

--- parseur.ml
+++ parseur.ml
@@ -8,12 +8,57 @@ let fichier = open_in "automate.txt" ;;(* ouvre le fichier*)
 
 let lecture fichier = input_line fichier;;
 
-let analyse  fichier =
-let rec lecture_aux s p = match s with parser
+let parseur  fichier =
+let rec parseur_aux s p = match s with parser
 |[<'' '>] -> p
 |[<''\n'>] -> p
-|[<''0'..'9'|'a'..'z'|'A'..'Z'|'('|')'  as n; f>] -> lecture_aux f (p^(char_to_string n))
-|[<>] -> p in lecture_aux fichier "";;
+|[<''0'..'9'|'a'..'z'|'A'..'Z'|'('|')'  as n; f>] -> parseur_aux f (p^(char_to_string n))
+|[<>] -> p in parseur_aux fichier "";;
 
 
-lecture fichier;;
+let rec indenter i sortie = 
+  while !i>0 do 
+    output_char sortie '\t';
+    i:=!i-1
+  done ;;
+
+let analyse () = 
+  let k = ref 0
+  in let i= ref 0
+     in let entree = open_in "automate.txt"
+	in let sortie = open_out "test" 
+	   in  if ((lecture entree)<> "/*automate*/")
+	     then failwith "erreur de fichier"
+	     else 
+	       while ((lecture entree)<> "fin")&&(!i<11) do
+		 indenter k sortie;
+		 output sortie "ecriture" 0 8;
+		 output_char sortie '\n';
+		 i:=!i+1;
+		 k:=!i
+	       done ;
+	     while ((lecture entree)<> "fin")&&(!i>0) do
+	       k:=!i;
+	       indenter k sortie;
+	       output sortie "ecriture" 0 8;
+	       output_char sortie '\n';
+	       i:=!i-1
+	     done ;
+	   close_out sortie;
+	   close_in entree;;
+
+
+  
+analyse();;
+
+
+
+
+let rec fact n = match n with 
+  |0->1
+  |k->k*(fact (k-1));;
+let n =ref 0 in
+while fact !n <= 1000 do
+  n:=!n+1
+done;
+!n;;
