1b8cd2e8be7c79cabf525bb86cd98edebd332cfe
Rémi Piotaix <remi@piotaix.fr>
2012-06-08 10:30:59 +0200

    Corrections

--- src/sim/tricycle/Ordonnanceur/Ordonnanceur.java
+++ src/sim/tricycle/Ordonnanceur/Ordonnanceur.java
@@ -16,10 +16,14 @@ import java.util.TimerTask;
  */
 public class Ordonnanceur implements OrdonnanceurInterface {
 
-    /* Tableau représentant les éléments qu'il reste à traiter */
-    private ArrayList<OrdonnancableInterface> subscribersActionToDo; 
-    /* Tableau représentant les éléments déjà traités */
-    private ArrayList<OrdonnancableInterface> subscribersActionDone; 
+    /*
+     * Tableau représentant les éléments qu'il reste à traiter
+     */
+    private ArrayList<OrdonnancableInterface> subscribersActionToDo;
+    /*
+     * Tableau représentant les éléments déjà traités
+     */
+    private ArrayList<OrdonnancableInterface> subscribersActionDone;
     private Timer timer;
     private long period = 500;
     private boolean running = false; // Par défaut l'ordonnaceur est sur "pause"
@@ -29,74 +33,87 @@ public class Ordonnanceur implements OrdonnanceurInterface {
      * Constructeur avec timer par défaut
      */
     public Ordonnanceur() {
-        subscribersActionToDo = new ArrayList();
-        subscribersActionDone = new ArrayList();
-        timer = new Timer();
-        timer.schedule(new TaskAction(), 0, period);
+        this(500);
     }
 
     /*
      * Constructeur avec timer en paramètre
      */
     public Ordonnanceur(long time) {
+        period = time;
         subscribersActionToDo = new ArrayList();
         subscribersActionDone = new ArrayList();
         timer = new Timer();
-        timer.schedule(new TaskAction(), 0, time);
+        timer.schedule(new TaskAction(), 0, period);
 
     }
-    
+
     /*
      * Exécute l'action de l'élément en paramètre
      */
-    private void doAction(OrdonnancableInterface oi){
+    private void doAction(OrdonnancableInterface oi) {
         oi.executeAction();
-    }
-    
-    /*
-     * Ajoute en fin de la liste des objets ayant des actions
-     * Utilisée pour transférer un élément de la liste à faire à celle de ceux faits
-     */
-    private void addToActionDone(OrdonnancableInterface oi){
-        subscribersActionDone.add(oi);   
-    }
-    
-   /*
-    * Enlève en tête de la liste des objets ayant des actions et le met dans la liste des actions effectuées
-    */
-    private void actionAndRemoveFromToDo(){
-        OrdonnancableInterface Elem = subscribersActionToDo.remove(0);
-        doAction(Elem);
-        addToActionDone(Elem);
     }
 
     /*
-     * Remplissage des actions à faire de manière aléatoire
-     * A faire une fois qu'il n'y a plus d'action à faire
+     * Ajoute en fin de la liste des objets ayant des actions Utilisée pour
+     * transférer un élément de la liste à faire à celle de ceux faits
      */
-    private void intializeActionToDo(){
+    private void addToActionDone(OrdonnancableInterface oi) {
+        subscribersActionDone.add(oi);
+    }
+
+    /*
+     * Enlève en tête de la liste des objets ayant des actions et le met dans la
+     * liste des actions effectuées
+     */
+    private void actionAndRemoveFromToDo() {
+        OrdonnancableInterface elem;
+        synchronized (this) {
+            elem = subscribersActionToDo.remove(0);
+            addToActionDone(elem);
+        }
+        doAction(elem);
+    }
+
+    /*
+     * Remplissage des actions à faire de manière aléatoire A faire une fois
+     * qu'il n'y a plus d'action à faire
+     */
+    private void intializeActionToDo() {
         int randomInt;
-        do{
-            /* Génération aléatoire d'un nombre */
+        while (!subscribersActionDone.isEmpty()) {
+            /*
+             * Génération aléatoire d'un nombre
+             */
             randomInt = randomGenerator.nextInt(1000);
-            /* On enlève un élément de la liste des actions faites, dont l'index est le modulo du nombre aléatoire par 
-             le nombre d'éléments restants dans la liste que l'on met dans la liste des actions à faire*/
-            add(subscribersActionDone.remove(randomInt%(subscribersActionDone.size())));
-            /* Tant que la liste des actions faites n'est pas vide*/
-        }while (!subscribersActionDone.isEmpty());
+            /*
+             * On enlève un élément de la liste des actions faites, dont l'index
+             * est le modulo du nombre aléatoire par le nombre d'éléments
+             * restants dans la liste que l'on met dans la liste des actions à
+             * faire
+             */
+            add(subscribersActionDone.remove(randomInt % (subscribersActionDone.size())));
+            /*
+             * Tant que la liste des actions faites n'est pas vide
+             */
+        }
         System.out.println("------- Remplissage des actions à faire -------");
     }
-    
+
     class TaskAction extends TimerTask {
 
         @Override
         public void run() {
             if (running) {
-                if (!subscribersActionToDo.isEmpty())
+                if (!subscribersActionToDo.isEmpty()) {
                     actionAndRemoveFromToDo();
-                else
-                    /* Plus d'action à faire donc on transvase les actions dans le tableau des actions à faire de manière aléatoire */
+                } else /*
+                 * Plus d'action à faire donc on transvase les actions dans le
+                 * tableau des actions à faire de manière aléatoire
+                 */ {
                     intializeActionToDo();
+                }
             }
         }
     }
@@ -106,21 +123,26 @@ public class Ordonnanceur implements OrdonnanceurInterface {
      * Enlève définitivement l'objet entré en paramètre
      */
     public void remove(OrdonnancableInterface oi) {
-        /* Si l'element à enlever est dans la liste des éléments restants à traiter on l'enlève (ex: un bot tue une autre bot, il ne faut 
-         pas que celui ci puisse faire une action une fois mort)*/
-        if (subscribersActionToDo.contains(oi))
-            subscribersActionToDo.remove(subscribersActionToDo.indexOf(oi));
-        /* Sinon on l'enlève des éléments déjà traités */
-        else
-            subscribersActionDone.remove(subscribersActionDone.indexOf(oi));
+        /*
+         * Si l'element à enlever est dans la liste des éléments restants à
+         * traiter on l'enlève (ex: un bot tue une autre bot, il ne faut pas que
+         * celui ci puisse faire une action une fois mort)
+         */
+        synchronized (this) {
+            if (subscribersActionToDo.contains(oi)) {
+                subscribersActionToDo.remove(oi);
+            } else {// Sinon on l'enlève des éléments déjà traités
+                subscribersActionDone.remove(oi);
+            }
+        }
     }
 
     /*
-     * Ajoute l'objet en paramètre à la fin de la liste des actions à faire
-     * Ex : Création nouveau bot
+     * Ajoute l'objet en paramètre à la fin de la liste des actions à faire Ex :
+     * Création nouveau bot
      */
     @Override
-    public void add(OrdonnancableInterface oi) {
+    public synchronized void add(OrdonnancableInterface oi) {
         subscribersActionToDo.add(oi);
     }
 
