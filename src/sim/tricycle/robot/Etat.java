78f40041882ef0860bd753c47c99b739f50aa336
Rémi Piotaix <remi@piotaix.fr>
2012-06-08 16:11:32 +0200

    getter and setter + attribut id

--- src/sim/tricycle/robot/Etat.java
+++ src/sim/tricycle/robot/Etat.java
@@ -11,12 +11,27 @@ import java.util.ArrayList;
  * @author Adri
  */
 public class Etat {
-    
+
+    private int id;
     private ArrayList<Transition> listeTransition;
-    
-    public Etat(){
-        
+
+    public Etat(int id) {
+        this.id = id;
     }
-    
-    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Transition> getListeTransition() {
+        return listeTransition;
+    }
+
+    public void setListeTransition(ArrayList<Transition> listeTransition) {
+        this.listeTransition = listeTransition;
+    }
 }
