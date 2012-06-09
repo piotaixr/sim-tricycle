18ef86073ef2e3a18382e6c7c5975897f9cff97b
Augustin Husson <husson.augustin@gmail.com>
2012-06-09 23:35:18 +0200

    OMG ^^ maj du .ml et de la syntaxe de l'automate: prise en compte des conditions et bientôt des etats

--- parseur.ml
+++ parseur.ml
@@ -7,7 +7,7 @@ type 'a pile= Pile_vide|Pile_non_vide of 'a*'a pile;;
 
 (* corps du .ml *)
 let sommet_pile p = match p with
-  |Pile_non_vide(s,p')->s
+  |Pile_non_vide(e,p')->e
   |Pile_vide->failwith "pile vide";;
 
 let empiler e p = Pile_non_vide(e,p);;
@@ -32,13 +32,90 @@ let rec indenter i sortie =
     i:=!i-1
   done ;;
 
+let rec parseur pile sortie stream nom= match stream with parser 
+  |[<'' ';f>] ->(parseur pile sortie f "");
+  |[<''\n';f>] ->(parseur pile sortie f "");
+  |[<''\t';f>]->(parseur pile sortie f "");
+  |[<''(';f>]-> (let name = nom 
+		 in match name with
+		   |"si"-> ecriture_aux sortie ("<condition nom=");
+		     let p= (empiler "condition" pile) in (parseur p sortie f "")
+		   |post-> parseur pile sortie f "")
+
+   |[<'')';f>]->(let name = (sommet_pile pile)
+		in match name with 
+		  |"condition"-> ecriture_aux sortie ("\""^nom^"\""^">");
+		    (ecriture_aux_char sortie '\n');
+		    (parseur pile sortie f "")
+		  |post->pile)
+
+  |[<''a'..'z'|'A'..'Z'|'='|'_'  as n; f>] -> parseur pile sortie f (nom^(char_to_string n))
+
+  |[<''}';f>]->let name = (sommet_pile pile)
+	     in (ecriture_aux sortie ("<"^"/"^name^">"));
+	       (ecriture_aux_char sortie '\n');
+	     let p=(depiler pile) in (parseur p sortie f "")
+				  
+  |[<''{';f>]-> (let name = nom 
+		in match name with
+		  |"sinon"->ecriture_aux sortie ("<condition nom="^"\""^"true"^"\""^">");
+		    (ecriture_aux_char sortie '\n');
+		    let p= (empiler "condition" pile) in (parseur p sortie f "")
+
+		  |""->pile
+
+		  |post-> ecriture_aux sortie ("<"^post^">");
+		    (ecriture_aux_char sortie '\n');
+		    let p= (empiler post pile) in (parseur p sortie f ""))
+   |[< >]->pile;;
+
+
+let analyse () = 
+  let entree = open_in "pile.txt"
+  in let sortie = open_out "test.xml" 
+	in if (lecture_aux entree)<>"/*fichier automate*/"
+	  then failwith "erreur de fichier"
+	  else let lect = (lecture_aux entree)
+	       in  let rec boucle pile lect = match lect with
+		 |"fin"->()
+		 |mot->let p=(parseur pile sortie(transf2 mot) "")
+			in (boucle p (lecture_aux entree))
+		  in boucle Pile_vide lect;
+     close_out sortie;
+     close_in entree;;
+
+analyse();;
+
+(*__________________________________________________________________________*)
+
+(* test pour transformation de fonction *)
+
+(*
+let entree = open_in "automate.txt";;
+let sortie = open_out "test";;
+lecture_aux entree;;
+let transf entree= Stream.of_string(input_line entree);;
+parseur sortie (transf fichier) "";;
+let ecriture_aux sortie  mot = output sortie mot 0 (String.length mot);;
+  (*close_out sortie;;*)
+ecriture_aux sortie "blabla";;
+
+close_out sortie;;
+close_in entree;;
+*)
+(*______________________________________________________________________*)
+
+(* fonction de base (pour revenir si y a un problème 
+objectivement ne sert pas )*)
+(*
+
 let rec parseur pile sortie string nom= match string with parser 
   |[<'' '>] ->pile
   |[<''\n'>] ->pile
-  |[<''\t';f>]-> (*ecriture_aux_char sortie '\t';*)(parseur pile sortie f nom)
+  |[<''\t';f>]-> (parseur pile sortie f nom)
   |[<''a'..'z'|'A'..'Z'|'('|')'|'='|'_'  as n; f>] -> parseur pile sortie f (nom^(char_to_string n))
   |[<''}';f>]->let name = (sommet_pile pile)
-	     in (ecriture_aux sortie ("<"^name^"/"^">"));
+	     in (ecriture_aux sortie ("<"^"/"^name^">"));
 	       (ecriture_aux_char sortie '\n');
 	     let p=(depiler pile) in (parseur p sortie f "")
 
@@ -47,7 +124,7 @@ let rec parseur pile sortie string nom= match string with parser
 
 let analyse () = 
   let entree = open_in "pile.txt"
-  in let sortie = open_out "test" 
+  in let sortie = open_out "test.xml" 
 	in if (lecture_aux entree)<>"/*fichier automate*/"
 	  then failwith "erreur de fichier"
 	  else let lect = (lecture_aux entree)
@@ -60,35 +137,4 @@ let analyse () =
      close_in entree;;
 
 analyse();;
-
-(*__________________________________________________________________________*)
-
-(* test pour transformation de fonction *)
-
-
-let entree = open_in "automate.txt";;
-let sortie = open_out "test";;
-lecture_aux entree;;
-let transf entree= Stream.of_string(input_line entree);;
-parseur sortie (transf fichier) "";;
-let ecriture_aux sortie  mot = output sortie mot 0 (String.length mot);;
-  (*close_out sortie;;*)
-ecriture_aux sortie "blabla";;
-
-close_out sortie;;
-close_in entree;;
-let inserer
-
-
-(*______________________________________________________________________*)
-
-(* fonction de base (pour revenir si y a un problème 
-objectivement ne sert pas )*)
-
-let parseur  fichier =
-let rec parseur_aux s p = match s with parser
-|[<'' '>] -> p
-|[<''\n'>] -> p
-|[<''0'..'9'|'a'..'z'|'A'..'Z'|'('|')'  as n; f>] -> parseur_aux f (p^(char_to_string n))
-|[<>] -> p in parseur_aux fichier "";;
-
+*)
