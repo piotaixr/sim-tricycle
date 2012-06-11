88d754e3c359dbf5bd80b2f1c9c9739a23e15963
Thomas Nunes <nds.thomas@gmail.com>
2012-06-11 15:30:15 +0200

    actualisation palcement frame top

--- src/sim/tricycle/ihm/FrameGame1.java
+++ src/sim/tricycle/ihm/FrameGame1.java
@@ -221,9 +221,9 @@ public class FrameGame1 extends javax.swing.JFrame {
                 .addComponent(buttonStepPlay)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(buttonSpeedUp)
-                .addGap(87, 87, 87)
+                .addGap(89, 89, 89)
                 .addComponent(scorePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 162, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 160, Short.MAX_VALUE)
                 .addComponent(buttonExit, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(25, 25, 25))
         );
