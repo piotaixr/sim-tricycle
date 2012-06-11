e6045476a1c5e81795d69a2b20599e633886b849
Morgan Bidois <morganbidois@gmail.com>
2012-06-11 10:43:03 +0200

    maj view

--- src/sim/tricycle/ihm/ViewCarte.java
+++ src/sim/tricycle/ihm/ViewCarte.java
@@ -26,7 +26,8 @@ public class ViewCarte extends javax.swing.JPanel {
     //private int decalageX;    //pour plus tard essayer de centrer la carte dans le JScrollPane
     //private int decalageY;
     private int tailleOpti;
-    private Image imgPiece,imgMur, imgVide, imgRobot, imgBonus, imgBoule;
+    private Image imgPiece, imgMur, imgVide, imgRobot, imgBonus, imgBoule;
+    private int px, py; //pour faire la difference lors du drag
 
     /**
      * Creates new form ViewCarte
@@ -48,7 +49,7 @@ public class ViewCarte extends javax.swing.JPanel {
             imgRobot = ImageIO.read(new File("./src/sim/tricycle/ihm/robot.jpg"));
             imgBonus = ImageIO.read(new File("./src/sim/tricycle/ihm/bonus.jpg"));
             imgBoule = ImageIO.read(new File("./src/sim/tricycle/ihm/boule.jpg"));
-            imgPiece = ImageIO.read(new File("./src/sim/tricycle/ihm/piece.jpg"));           
+            imgPiece = ImageIO.read(new File("./src/sim/tricycle/ihm/piece.jpg"));
 
         } catch (IOException ex) {
             Logger.getLogger(ViewCarte.class.getName()).log(Level.SEVERE, null, ex);
@@ -135,6 +136,17 @@ public class ViewCarte extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                formMousePressed(evt);
+            }
+        });
+        addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                formMouseDragged(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -146,6 +158,26 @@ public class ViewCarte extends javax.swing.JPanel {
             .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseDragged
+        // TODO add your handling code here:
+        System.out.println("dragged");
+        //final Component t = evt.getComponent();
+        //evt.translatePoint(getLocation().x + t.getLocation().x - px, getLocation().y + t.getLocation().y - py);
+        this.setLocation(evt.getLocationOnScreen().x - px, evt.getLocationOnScreen().y - py);
+        px = evt.getLocationOnScreen().x - this.getX();
+        py = evt.getLocationOnScreen().y - this.getY();
+        this.getParent().validate(); //A mettre en commentaire si on veut bouger la map comme on veut mais avec les pb de dessin...
+    }//GEN-LAST:event_formMouseDragged
+
+    private void formMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMousePressed
+        // TODO add your handling code here:
+        //evt.translatePoint(evt.getComponent().getLocation().x, evt.getComponent().getLocation().y);
+        px = evt.getLocationOnScreen().x - this.getX();
+        py = evt.getLocationOnScreen().y - this.getY();
+
+        System.out.println("click position : " + px + " " + py);
+    }//GEN-LAST:event_formMousePressed
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }
