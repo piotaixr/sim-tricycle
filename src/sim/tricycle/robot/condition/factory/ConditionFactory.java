f54ca55cb14befbdb3170fa9406ee1d760d7dadd
Rémi Piotaix <remi@piotaix.fr>
2012-06-11 16:00:42 +0200

    Refractor pour les factory construisant des objets en leur passant des paramètres

--- src/sim/tricycle/robot/condition/factory/ConditionFactory.java
+++ src/sim/tricycle/robot/condition/factory/ConditionFactory.java
@@ -4,14 +4,13 @@
  */
 package sim.tricycle.robot.condition.factory;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.*;
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.jdom2.Element;
 import sim.tricycle.robot.condition.ConditionInterface;
-import sim.tricycle.robot.condition.ParameterCreator;
+import sim.tricycle.utils.AbstractFactory;
 import sim.tricycle.utils.params.ParamConverterProviderInterface;
 import sim.tricycle.utils.params.Parameter;
 
@@ -19,32 +18,25 @@ import sim.tricycle.utils.params.Parameter;
  *
  * @author Rémi PIOTAIX <remi.piotaix@gmail.com>
  */
-public class ConditionFactory implements ConditionFactoryInterface {
-
-    private HashMap<String, ConditionInterface> conditions = new HashMap<String, ConditionInterface>();
-    private ParamConverterProviderInterface paramConverterProvider;
+public class ConditionFactory extends AbstractFactory<String, ConditionInterface> implements ConditionFactoryInterface {
 
     public ConditionFactory(ParamConverterProviderInterface paramConverterProvider) {
-        this.paramConverterProvider = paramConverterProvider;
+        super(paramConverterProvider);
     }
 
-    
     @Override
-    public ConditionInterface create(String nom, List<Parameter> parameters) {
+    public ConditionInterface doCreate(String nom, List<Parameter> parameters) {
         ConditionInterface returnValue = null;
         try {
-            if (!conditions.containsKey(nom)) {
-                throw new RuntimeException("La condition de nom " + nom + " n'existe pas ou n'est pas enregistrée auprès du système.");
-            }
+            ConditionInterface obj = getValues().get(nom);
+            ConditionInterface clone = obj.clone();
+            Class c = obj.getClass();
 
-            Class c = conditions.get(nom).getClass();
+            Method method = c.getMethod("setParameters", toRequiredTypes(parameters));
 
-            Constructor construct = c.getConstructor(toRequiredTypes(parameters));
+            method.invoke(clone, toConvertedValues(parameters));
 
-            returnValue = (ConditionInterface) construct.newInstance(toConvertedValues(parameters));
-
-            return null;
-        } catch (InstantiationException ex) {
+        } catch (CloneNotSupportedException ex) {
             Logger.getLogger(ConditionFactory.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
             Logger.getLogger(ConditionFactory.class.getName()).log(Level.SEVERE, null, ex);
@@ -61,47 +53,4 @@ public class ConditionFactory implements ConditionFactoryInterface {
         return returnValue;
     }
 
-    @Override
-    public void registerConditions(Collection<ConditionInterface> conditions) {
-        for (ConditionInterface c : conditions) {
-            registerCondition(c);
-        }
-    }
-
-    @Override
-    public void registerCondition(ConditionInterface condition) {
-        if (conditions.containsKey(condition.getName())) {
-            throw new RuntimeException("Une condition doit avoir un nom unique: " + condition.getName());
-        }
-
-        conditions.put(condition.getName(), condition);
-    }
-
-    private Class[] toRequiredTypes(List<Parameter> parameters) {
-        int nombre = parameters.size();
-        Class[] types = new Class[nombre];
-        int i = 0;
-        Iterator<Parameter> it = parameters.iterator();
-        while (it.hasNext()) {
-            Parameter param = it.next();
-
-            types[i++] = paramConverterProvider.get(param.getType()).getOutputClass();
-        }
-
-        return types;
-    }
-
-    private Object[] toConvertedValues(List<Parameter> parameters) {
-        int nombre = parameters.size();
-        Object[] values = new Object[nombre];
-        int i = 0;
-        Iterator<Parameter> it = parameters.iterator();
-        while (it.hasNext()) {
-            Parameter param = it.next();
-
-            values[i++] = paramConverterProvider.get(param.getType()).convert(param.getValue());
-        }
-
-        return values;
-    }
 }
--- src/sim/tricycle/robot/condition/factory/ConditionFactoryInterface.java
+++ src/sim/tricycle/robot/condition/factory/ConditionFactoryInterface.java
@@ -3,17 +3,20 @@ package sim.tricycle.robot.condition.factory;
 import java.util.Collection;
 import java.util.List;
 import sim.tricycle.robot.condition.ConditionInterface;
+import sim.tricycle.utils.FactoryInterface;
 import sim.tricycle.utils.params.Parameter;
 
 /**
  *
  * @author Rémi PIOTAIX <remi.piotaix@gmail.com>
  */
-public interface ConditionFactoryInterface {
+public interface ConditionFactoryInterface extends FactoryInterface<String, ConditionInterface> {
 
     public ConditionInterface create(String nom, List<Parameter> parameters);
 
-    public void registerConditions(Collection<ConditionInterface> conditions);
+    @Override
+    public void registerCollection(Collection<ConditionInterface> conditions);
 
-    public void registerCondition(ConditionInterface condition);
+    @Override
+    public void register(ConditionInterface condition);
 }
