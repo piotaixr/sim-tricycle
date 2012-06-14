df3d5c63092a54bdccb9340421ae78b9a1ebeed0
Rémi Piotaix <remi@piotaix.fr>
2012-06-14 15:36:55 +0200

    converters pour reference et variables

---
+++ src/sim/tricycle/utils/params/converter/VariableConverter.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sim.tricycle.utils.params.converter;
+
+import sim.tricycle.Ordonnanceur.OrdonnanceurInterface;
+import sim.tricycle.utils.params.ParamConverterInterface;
+import sim.tricycle.utils.params.types.Variable;
+
+public class VariableConverter implements ParamConverterInterface {
+
+    private OrdonnanceurInterface ordonnanceur;
+
+    public VariableConverter(OrdonnanceurInterface ordonnanceur) {
+        this.ordonnanceur = ordonnanceur;
+    }
+
+    @Override
+    public Object convert(String chaine) {
+        return new Variable(chaine.trim(), ordonnanceur);
+    }
+
+    @Override
+    public String getName() {
+        return "variable";
+    }
+
+    @Override
+    public Class getOutputClass() {
+        return Variable.class;
+    }
+}
