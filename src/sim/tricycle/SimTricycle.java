9984fabfecd24350208bf08efa0ae344431675f2
Thomas Nunes <nds.thomas@gmail.com>
2012-06-18 11:12:15 +0200

    minimap moins gourmande!

--- src/sim/tricycle/SimTricycle.java
+++ src/sim/tricycle/SimTricycle.java
@@ -30,8 +30,8 @@ public class SimTricycle {
      */
     public static void main(String[] args) {
 
- //       MapTest cr = new MapTest();
-   //     Carte c = cr.getCarte();
+        //MapTest cr = new MapTest();
+       // Carte c = cr.getCarte();
         CrossRiver cr = new CrossRiver();
         Carte c = cr.getCarte();
         sim.tricycle.team.Team t = new sim.tricycle.team.Team("Winneurs", c, new Point(0, 0), new ArrayList<Ressource>());
