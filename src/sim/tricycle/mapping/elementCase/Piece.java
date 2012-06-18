8e9ef3b452c7aa25e3e86e426bf24c7b9c45a4a3
Rémi Piotaix <remi@piotaix.fr>
2012-06-19 01:02:28 +0200

    constructeur par defaut pour piece

--- src/sim/tricycle/mapping/elementCase/Piece.java
+++ src/sim/tricycle/mapping/elementCase/Piece.java
@@ -1,19 +1,12 @@
 package sim.tricycle.mapping.elementCase;
 
-import sim.tricycle.mapping.Case;
 import sim.tricycle.mapping.TypeCase;
-import sim.tricycle.mapping.mapException.CaseMultipleObjetException;
 
 /**
  *
  * @author Thomas Nds nds.thomas@gmail.com
  */
 public class Piece extends AbstractObjet {
-
-    public Piece(Case pos) {
-        this.pos = pos;
-        pos.setItem(this);
-    }
 
     @Override
     public TypeCase whoIam() {
