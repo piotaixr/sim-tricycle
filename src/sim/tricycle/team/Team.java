df242bc6c41815373761775e0c30feedd116f142
Thomas Nunes <nds.thomas@gmail.com>
2012-06-19 11:14:12 +0200

    base et team modifié pour utilsiation de la nouvelle base.

--- src/sim/tricycle/team/Team.java
+++ src/sim/tricycle/team/Team.java
@@ -7,32 +7,43 @@ import java.util.LinkedList;
 import sim.tricycle.mapping.CarteTeam;
 import sim.tricycle.mapping.Case;
 import sim.tricycle.mapping.elementCase.AbstractObjet;
+import sim.tricycle.mapping.elementCase.Base;
 import sim.tricycle.robot.Point;
 
 /**
- * @author Marion Dalle 
- * @author AdriA */
+ * @author Marion Dalle
+ * @author AdriA
+ */
 public class Team {
 
     private String nomTeam;
     private LinkedList<Robot> armee;
     private LinkedList<Robot> models;
     private CarteTeam map;
-    private Point base;
     private ArrayList<Ressource> ressources;
     private ArrayList<Case> collectables;
-    private Color color=Color.cyan;
+    private Color color = Color.cyan;
+    private Base base=null;
 
-    
-    public Team(String nomTeam, CarteTeam map, Point base, ArrayList<Ressource> ressources ) {
+
+    public Team(String nomTeam, CarteTeam map, Case posBase) {
         this.nomTeam = nomTeam;
         this.map = map;
         this.armee = new LinkedList<Robot>();
+        this.ressources = new ArrayList<Ressource>();
+        this.base= new Base();
+        this.base.setCase(posBase);
+        this.base.setT(this);
+    }
+    
+    public void setBase(Base base) {
         this.base = base;
-        this.ressources = ressources;
-        
     }
 
+    public Base getBase() {
+        return base;
+    }
+    
     public void addRobot(Robot bot) {
         this.armee.add(bot);
     }
@@ -45,14 +56,6 @@ public class Team {
         return this.nomTeam;
     }
 
-    public void setBase(Point newBase) {
-        this.base = newBase;
-    }
-
-    public Point getBase() {
-        return this.base;
-    }
-    
     public ArrayList<Ressource> getRessources() {
         return ressources;
     }
@@ -60,41 +63,41 @@ public class Team {
     public void setRessources(ArrayList<Ressource> ressources) {
         this.ressources = ressources;
     }
-    
-    /* Au secours j'y comprend rien */
-    public Ressource trouveRessourceParItem (AbstractObjet item){        
-        return ressources.get(ressources.indexOf(new Ressource (item,0)));
+
+    /*
+     * Au secours j'y comprend rien
+     */
+    public Ressource trouveRessourceParItem(AbstractObjet item) {
+        return ressources.get(ressources.indexOf(new Ressource(item, 0)));
     }
-    
-    public void ajouterRessource(AbstractObjet item){
-        
-        Ressource r = new Ressource(item,1);
-        
-        if(this.ressources.contains(r)){
-            r=trouveRessourceParItem(item);
-            r.setQuantite(r.getQuantite()+1);
-        }else{
+
+    public void ajouterRessource(AbstractObjet item) {
+
+        Ressource r = new Ressource(item, 1);
+
+        if (this.ressources.contains(r)) {
+            r = trouveRessourceParItem(item);
+            r.setQuantite(r.getQuantite() + 1);
+        } else {
             this.ressources.add(r);
         }
     }
 
-    public void supprimerRessource(AbstractObjet item, int q){
-        Ressource r = new Ressource(item,0);
-        
-        if (this.ressources.contains(r)){
-            r=trouveRessourceParItem(item);
-            if (r.getQuantite()>=q){
-            r.setQuantite(r.getQuantite()-q);
-            }
-            else {
+    public void supprimerRessource(AbstractObjet item, int q) {
+        Ressource r = new Ressource(item, 0);
+
+        if (this.ressources.contains(r)) {
+            r = trouveRessourceParItem(item);
+            if (r.getQuantite() >= q) {
+                r.setQuantite(r.getQuantite() - q);
+            } else {
                 throw new RuntimeException("quantité insuffisante");
             }
-        }
-        else{
+        } else {
             throw new RuntimeException("quantité insuffisante");
         }
     }
-    
+
     public LinkedList<Robot> getArmee() {
         return armee;
     }
@@ -103,7 +106,7 @@ public class Team {
         this.armee = armee;
     }
 
-     public ArrayList<Case> getCollectables() {
+    public ArrayList<Case> getCollectables() {
         return collectables;
     }
 
@@ -118,35 +121,35 @@ public class Team {
     public void setColor(Color color) {
         this.color = color;
     }
-    
-    public Case getCollectableCiblable(){
-        
-        int i=0;
-        
-        while(i<this.collectables.size() && !this.collectables.get(i).isCiblable()){
+
+    public Case getCollectableCiblable() {
+
+        int i = 0;
+
+        while (i < this.collectables.size() && !this.collectables.get(i).isCiblable()) {
             i++;
         }
-        
-        if(i>=this.collectables.size()){
+
+        if (i >= this.collectables.size()) {
             return null;
-        }else{
+        } else {
             return this.collectables.get(i);
         }
     }
-    
-    public LinkedList<Robot> getModel(){
+
+    public LinkedList<Robot> getModel() {
         return models;
     }
-    
-    public void setModel(LinkedList<Robot> mod){
+
+    public void setModel(LinkedList<Robot> mod) {
         models = mod;
     }
-    
-    public void addModel(Robot rob){
+
+    public void addModel(Robot rob) {
         models.add(rob);
     }
-    
-    public void removeModel(){
+
+    public void removeModel() {
         models.remove();
     }
 }
