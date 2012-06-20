9faeb0a0f7ab5e2a63584265e65b00076d794def
Thomas Nunes <nds.thomas@gmail.com>
2012-06-20 17:53:05 +0200

    corection d'affichage. Je suis de retour!! Vu qu'il restez peu de tps je suis rentrer chez moi :p

--- src/sim/tricycle/ihm/ViewCarte.java
+++ src/sim/tricycle/ihm/ViewCarte.java
@@ -30,8 +30,8 @@ public class ViewCarte extends javax.swing.JPanel {
     private int tailleOpti;
     private Image img, imgMap, imgVide;
     private int px, py; //pour faire la difference lors du drag
-    private HashMap<String, Image> ensaCharger = null;
-    private HashMap<String, Image> enstteCase = null;
+    private HashMap<String, Image> ensaCharger = new HashMap<String, Image>();
+    private HashMap<String, Image> enstteCase = new HashMap<String, Image>();
 
     /**
      * Constructeur de carte implémentées
@@ -54,7 +54,7 @@ public class ViewCarte extends javax.swing.JPanel {
             // Initialisation des images:
             FilesFinder ff = new FilesFinder();
             ArrayList<String> aCharger = ff.findFilesWithExtensions("./src/sim/tricycle/ihm/images/imageAcharger");
-            ArrayList<String> tteCase = ff.findFilesWithExtensions("./src/sim/tricycle/ihm/images/imageAcharger");
+            ArrayList<String> tteCase = ff.findFilesWithExtensions("./src/sim/tricycle/ihm/images/cases/");
 
             for (String x : aCharger) {
                 ensaCharger.put(x, ImageIO.read(new File("./src/sim/tricycle/ihm/images/imageAcharger/" + x)));
@@ -63,13 +63,6 @@ public class ViewCarte extends javax.swing.JPanel {
             for (String x : tteCase) {
                 enstteCase.put(x, ImageIO.read(new File("./src/sim/tricycle/ihm/images/cases/" + x)));
             }
-
-//            imgBase = ImageIO.read(new File("./src/sim/tricycle/ihm/images/base.png"));
-//            imgRobot = ImageIO.read(new File("./src/sim/tricycle/ihm/images/robotCR.png"));
-//            imgBonus = ImageIO.read(new File("./src/sim/tricycle/ihm/images/cases/bonus.png"));
-//            imgBoule = ImageIO.read(new File("./src/sim/tricycle/ihm/images/cases/boule.png"));
-//            imgPiece = ImageIO.read(new File("./src/sim/tricycle/ihm/images/cases/piece.png"));
-//            imgPT = ImageIO.read(new File("./src/sim/tricycle/ihm/images/cases/A5.png"));
         } catch (IOException ex) {
             Logger.getLogger(ViewCarte.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -79,10 +72,7 @@ public class ViewCarte extends javax.swing.JPanel {
     public void paint(Graphics graphic) {
         super.paint(graphic);
         Graphics2D g = (Graphics2D) graphic;
-
         Dimension d = new Dimension(carte.getLargeur() * tailleCase, carte.getHauteur() * tailleCase);
-
-        //this.setPreferredSize(this.getSize());
         this.setPreferredSize(d);
 
         decalageX = 0;
@@ -112,7 +102,6 @@ public class ViewCarte extends javax.swing.JPanel {
                 paintCase(g, carte.getCase(i, j), tailleCase, true, affFond);
             }
         }
-
     }
 
     /**
@@ -143,12 +132,15 @@ public class ViewCarte extends javax.swing.JPanel {
         } else if (c.whoIam() == TypeCase.mur) {                             //MUR
             // SI pas de map de fond => on affiche les murs.
             if (aff) {
-                g.drawImage(enstteCase.get(c.getId() + ".png"), x, y, width, width, this);
+               // g.drawImage(enstteCase.get(c.getId() + ".png"), x, y, width, width, this);
+                g.drawImage(enstteCase.get("X.jpg"), x, y, width, width, this);
             }
+
         } else {                                                            //VIDE
             char ch = c.getId().charAt(0);
             if (ch == 'A' || ch == 'C') {   //Si case à motif 
                 // on recupere l'image corespondante à l'id.
+                g.drawImage(imgVide, x, y, width, width, this);
                 g.drawImage(enstteCase.get(c.getId() + ".png"), x, y, width, width, this);
             } else {
                 g.drawImage(imgVide, x, y, width, width, this);
@@ -184,7 +176,7 @@ public class ViewCarte extends javax.swing.JPanel {
                 g.drawImage(enstteCase.get("A5.png"), x, y, width, width, this);
             }
             if (c.getZone().whoIam() == TypeCase.base) {
-                 g.drawImage(ensaCharger.get("base.png"), x, y, width, width, this);
+                g.drawImage(ensaCharger.get("base.png"), x, y, width, width, this);
             }
 
         }
