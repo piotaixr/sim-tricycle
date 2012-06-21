70bebe4e28c255e7ce5e4df0c9f20f2f9aae4e90
Adrien Ortola <ortola66@gmail.com>
2012-06-21 16:55:14 +0200

    test automate de trajet

--- src/sim/tricycle/TestParser.java
+++ src/sim/tricycle/TestParser.java
@@ -20,7 +20,7 @@ public class TestParser {
 
         RobotParser parser = builder.getRobotParser();
         System.out.println("aaaa");
-        parser.parse(new File("./test_basique.xml"));
-        parser.parse(new File("./test.xml"));
+     //   parser.parse(new File("./test_basique.xml"));
+        parser.parse(new File("./Automates/trajet.xml"));
     }
 }
