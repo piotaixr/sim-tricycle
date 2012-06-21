2c2ade8a8d9843231ae94a6464bb637e726312fb
Rémi Piotaix <remi@piotaix.fr>
2012-06-21 16:59:43 +0200

    maj test

--- src/sim/tricycle/TestParser.java
+++ src/sim/tricycle/TestParser.java
@@ -19,8 +19,8 @@ public class TestParser {
 
 
         RobotParser parser = builder.getRobotParser();
-        System.out.println("aaaa");
-        parser.parse(new File("./test_basique.xml"));
+        //System.out.println("aaaa");
+        //parser.parse(new File("./test_basique.xml"));
         parser.parse(new File("./test.xml"));
     }
 }
